fb47697041beee766551160fafb61b05bc5c417b
Swapnil Bhosale <bhosaleswapnil341@gmail.com>
2026-03-22 19:38:37 +0530

    Added object.js file

---
+++ Class/object.js
@@ -0,0 +1,46 @@
+//We can declare object using two types 
+// 1] object literal
+// 2] object singleton
+
+
+// 1]object literal
+let user = {
+    name: "Swapnil",
+    age: 20,
+    email:"swapnil@gmail.com",
+    city:"miraj",
+    isLogin: true,
+    lastLoginDate : ['sun','Mon','Tues','Wed'],
+    "full Name" : "Swapnil Bhosale"
+}
+console.log(user);
+console.log(user.email);
+console.log(user.lastLoginDate);
+console.log(user["full Name"]);
+
+user.email = "user@gmail.com";
+console.log(user.email);
+//Object.freeze(user);//Freeze Value
+user.email = "abc@gmail.com";
+console.log(user.email);
+
+//Symbol Example
+const mysymbol=Symbol("WT")
+const myobj1={
+    [mysymbol]:"Hello",
+
+}
+console.log(myobj1);
+console.log(typeof(myobj1));
+
+user.greeting = function(){
+    console.log("Hello.js");
+    
+}
+console.log(user.greeting());
+
+user.greeting2 = function(){
+    console.log(`email : ${this.email}`);
+    
+}
+console.log(user.greeting2());
