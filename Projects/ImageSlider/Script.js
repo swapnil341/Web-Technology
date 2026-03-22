0f3b05b16a3913da81ce5f4b258deefcd28fe349
Swapnil Bhosale <bhosaleswapnil341@gmail.com>
2026-03-22 18:56:51 +0530

    removing online image references

--- Projects/ImageSlider/Script.js
+++ Projects/ImageSlider/Script.js
@@ -1,10 +1,3 @@
-// const images = [
-//     "https://picsum.photos/id/1015/600/400",
-//     "https://picsum.photos/id/1016/600/400",
-//     "https://picsum.photos/id/1018/600/400",
-//     "https://picsum.photos/id/1020/600/400"
-// ];
-
 const images = [
     "Images/image1.jpg",
     "Images/image2.jpg",
