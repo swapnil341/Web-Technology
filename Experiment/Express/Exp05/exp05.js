ef60bc944b4503cbcc02afb8bfd65f95659b9ed3
Swapnil Bhosale <bhosaleswapnil341@gmail.com>
2026-04-09 19:51:07 +0530

    Add comments about Express.js
    Added comments explaining the purpose of Express framework.

--- Experiment/Express/Exp05/exp05.js
+++ Experiment/Express/Exp05/exp05.js
@@ -1,3 +1,8 @@
+/*
+- Express is a minimal and flexible web application framework for node js that simplifies development of web servers and api.
+- It provides services as routing middleware support, request and response handling , rest api creation.
+*/
+
 const express = require('express')
 const app = express()
 const port = 3000
